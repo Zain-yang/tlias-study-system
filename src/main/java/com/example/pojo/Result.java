612384da0cbd489da667472c7aee1bef2152b758
Zain-yang <1794836904@qq.com>
2025-12-04 11:36:11 +0800

    first commit

---
+++ src/main/java/com/example/pojo/Result.java
@@ -0,0 +1,33 @@
+package com.example.pojo;
+
+import lombok.Data;
+
+@Data
+public class Result {
+    private Integer code;
+    private String msg;
+    private Object data;
+
+    public static Result success(){
+        Result result = new Result();
+        result.code = 200;
+        result.msg = "操作成功";
+
+        return result;
+    }
+
+    public static Result success(Object data){
+        Result result = new Result();
+        result.code = 200;
+        result.msg = "操作成功";
+        result.data = data;
+        return result;
+    }
+
+    public static Result error(String msg){
+        Result result = new Result();
+        result.code = 0;
+        result.msg = msg;
+        return result;
+    }
+}
